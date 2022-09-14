64583281f6e5550f7b36e24ca463a1168f1352c2
NC-KOREA\ekcho <ekcho@ncits.co.kr>
2022-09-14 23:07:38 +0900

    add : test case

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -21,5 +21,14 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    //public List<Order> findAll(OrderSearch orderSearch){}
+    public List<Order> findAll(OrderSearch orderSearch){
+        return em.createQuery("select o from Order o join o.member m" +
+                        " where o.status = :status" +
+                        " and m.username like :username",Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("username", orderSearch.getMemberName())
+                //.setFirstResult(100) paging
+                .setMaxResults(1000) // 최대 1000건
+                .getResultList();
+    }
 }
--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,11 +21,23 @@ public class ItemService {
         itemRepository.save(item);
     }
 
-    public List<Item> findItem(){
+    @Transactional
+    public void updateItem(Long itemId, Book param){
+        // 영속 상태이므로 repository.save 할필요 없음
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(param.getPrice());
+        findItem.setName(param.getName());
+        findItem.setStockQuantity(param.getStockQuantity());
+
+        // 값이 다 세팅이되면 @transactional에 의해서 commit -> flush
+    }
+
+    public List<Item> findItems(){
         return itemRepository.findAll();
     }
 
     public Item findOne(Long itemId){
         return itemRepository.findOne(itemId);
     }
+
 }
